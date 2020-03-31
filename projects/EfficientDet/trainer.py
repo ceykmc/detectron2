13c06d6a5639bc617202639e030f0316b58490c6
李军 <lj@xingzheyi.com>
2020-03-31 19:33:52 +0800

    支持evaluation

--- projects/EfficientDet/trainer.py
+++ projects/EfficientDet/trainer.py
@@ -1,7 +1,22 @@
 # -*- coding: utf-8 -*-
 
+import os
+import torch
+
+import detectron2.utils.comm as comm
 from detectron2.config import CfgNode
+from detectron2.data import MetadataCatalog
 from detectron2.engine import DefaultTrainer
+from detectron2.evaluation import (
+    CityscapesEvaluator,
+    COCOEvaluator,
+    COCOPanopticEvaluator,
+    DatasetEvaluators,
+    LVISEvaluator,
+    PascalVOCDetectionEvaluator,
+    SemSegEvaluator,
+    verify_results,
+)
 
 from modeling import build_model
 from utils import EMA
@@ -22,3 +37,47 @@ class EfficientDetTrainer(DefaultTrainer):
     @classmethod
     def build_model(cls, cfg: CfgNode):
         return build_model(cfg)
+
+    @classmethod
+    def build_evaluator(cls, cfg, dataset_name, output_folder=None):
+        """
+        Create evaluator(s) for a given dataset.
+        This uses the special metadata "evaluator_type" associated with each builtin dataset.
+        For your own dataset, you can simply create an evaluator manually in your
+        script and do not have to worry about the hacky if-else logic here.
+        """
+        if output_folder is None:
+            output_folder = os.path.join(cfg.OUTPUT_DIR, "inference")
+        evaluator_list = []
+        evaluator_type = MetadataCatalog.get(dataset_name).evaluator_type
+        if evaluator_type in ["sem_seg", "coco_panoptic_seg"]:
+            evaluator_list.append(
+                SemSegEvaluator(
+                    dataset_name,
+                    distributed=True,
+                    num_classes=cfg.MODEL.SEM_SEG_HEAD.NUM_CLASSES,
+                    ignore_label=cfg.MODEL.SEM_SEG_HEAD.IGNORE_VALUE,
+                    output_dir=output_folder,
+                ))
+        if evaluator_type in ["coco", "coco_panoptic_seg"]:
+            evaluator_list.append(
+                COCOEvaluator(dataset_name, cfg, True, output_folder))
+        if evaluator_type == "coco_panoptic_seg":
+            evaluator_list.append(
+                COCOPanopticEvaluator(dataset_name, output_folder))
+        elif evaluator_type == "cityscapes":
+            assert (
+                torch.cuda.device_count() >= comm.get_rank()
+            ), "CityscapesEvaluator currently do not work with multiple machines."
+            return CityscapesEvaluator(dataset_name)
+        elif evaluator_type == "pascal_voc":
+            return PascalVOCDetectionEvaluator(dataset_name)
+        elif evaluator_type == "lvis":
+            return LVISEvaluator(dataset_name, cfg, True, output_folder)
+        if len(evaluator_list) == 0:
+            raise NotImplementedError(
+                "no Evaluator for the dataset {} with the type {}".format(
+                    dataset_name, evaluator_type))
+        elif len(evaluator_list) == 1:
+            return evaluator_list[0]
+        return DatasetEvaluators(evaluator_list)
